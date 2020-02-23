2966d3069e4e12e7f112c9c9024380892d1ced11
ReydVires <ahmad.fire@gmail.com>
2020-02-23 19:32:27 +0700

    update author log

--- webpack/credits.js
+++ webpack/credits.js
@@ -1,1 +1,6 @@
-console.log('https://github.com/ReydVires');
+console.info(
+    "%c %c   ReydVires@2020 | Eko Protect 1.0.0 |   %c ",
+    "background:#353AFB",
+    "background:#000080;color:#fff",
+    "background:#353AFB"
+);
