217eacf87194d1166dbe2915126d970b310219e5
ReydVires <ahmad.fire@gmail.com>
2020-03-05 09:41:43 +0700

    add mapping data approach

--- src/scripts/objects/Tile.ts
+++ src/scripts/objects/Tile.ts
@@ -2,10 +2,13 @@ import { PhysicSprite } from "./abstract/PhysicSprite";
 
 export class Tile extends PhysicSprite {
 
-	constructor (scene: Phaser.Scene, x: number, y: number, texture: string) {
+	constructor (scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string) {
 		super(scene, x, y, texture, true);
 		this.setOrigin(0)
 			.setVisible(false);
+		if (frame) {
+			this.setFrame(frame);
+		}
 	}
 
 }
