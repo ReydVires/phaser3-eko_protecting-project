13d94c99e71e6c4640bce683bcd284e2a6cd0ca5
ReydVires <ahmad.fire@gmail.com>
2020-04-08 02:04:57 +0700

    fixing scrollable component on test

--- src/scripts/scenes/LeaderboardScene.ts
+++ src/scripts/scenes/LeaderboardScene.ts
@@ -38,6 +38,18 @@ export class LeaderboardScene extends Phaser.Scene {
 			<Betatester> {
 				name: 'POTTERS',
 				score: 100
+			},
+			<Betatester> {
+				name: 'SHAWNE',
+				score: 89
+			},
+			<Betatester> {
+				name: 'FRANK',
+				score: 60
+			},
+			<Betatester> {
+				name: 'MOORE',
+				score: 50
 			}
 		);
 	}
@@ -59,43 +71,64 @@ export class LeaderboardScene extends Phaser.Scene {
 		lbMask.setPosition(lbMask.x - (maskWidth * 0.5), lbMask.y - (maskHeight * 0.5));
 		// everySprite.setMask(new Phaser.Display.Masks.GeometryMask(this, lbMask));
 
-		// Scrollable
+		// Scrollable here!
 		let dataScoreText = `#${(1)}    ${this._betaTester[0].name}        ${this._betaTester[0].score}`;
 		for (let i = 1; i < this._betaTester.length; i++) {
 			const data = this._betaTester[i];
 			dataScoreText += `\n#${(i+1)}    ${data.name}        ${data.score}`;
 		}
-		const testText = this.add.text(0, 0, dataScoreText, { color: 'black' })
-			.setFontFamily('Comfortaa')
-			.setFontSize(28)
-			.setLineSpacing(40)
-			.setOrigin(0.5, 0);
-		console.log('testText height', testText.displayHeight);
+
+		const testText = this.add.text(0, 0, dataScoreText, { color: 'black' });
+		testText.setFontFamily('Comfortaa');
+		testText.setFontSize(28);
+		testText.setLineSpacing(40);
+		testText.setOrigin(0.5, 0);
 		testText.setMask(new Phaser.Display.Masks.GeometryMask(this, lbMask));
+
 		this._testerContainer = this.add.container(centerX, centerY - 128);
 		this._testerContainer.add(testText);
 
+		// Scroll property
 		let startTouch = new Phaser.Math.Vector2(0, 0);
 		let onTouchMove = new Phaser.Math.Vector2(0, 0);
 		let targetObject = new Phaser.Math.Vector2(this._testerContainer.x, this._testerContainer.y);
+
+		// Threshold of scrollable
+		const elementHeight = 77;
+		const canSwipe = this._betaTester.length > 5;
+		const thresholdDown = canSwipe ? (this._betaTester.length - 5) * elementHeight : 0;
+		const thresholdUp = 232;
+
 		this.input.on('pointerdown', (pointer: PointerEvent) => {
-			startTouch = new Phaser.Math.Vector2(pointer.x, pointer.y);
+			if (canSwipe) {
+				startTouch = new Phaser.Math.Vector2(pointer.x, pointer.y);
+			}
 		})
 		.on('pointermove', (pointer: PointerEvent) => {
-			onTouchMove = new Phaser.Math.Vector2(pointer.x, pointer.y);
-			let newY = (onTouchMove.y - startTouch.y) + targetObject.y;
-			newY = newY < 232 ? newY : 232;
-			newY = testText.displayHeight - newY < 165 ? newY : 165;
-			this._testerContainer.setY(Math.round(newY));
+			if (canSwipe) {
+				onTouchMove = new Phaser.Math.Vector2(pointer.x, pointer.y);
+				let newY = (onTouchMove.y - startTouch.y) + targetObject.y;
+				if (newY > thresholdUp) {
+					newY = thresholdUp;
+				}
+				else if (newY < (thresholdUp - thresholdDown)) {
+					newY = thresholdUp - thresholdDown;
+				}
+				this._testerContainer.setY(Math.round(newY));
+			}
 		})
 		.on('pointerup', () => {
-			targetObject = new Phaser.Math.Vector2(this._testerContainer.x, this._testerContainer.y);
+			if (canSwipe) {
+				targetObject = new Phaser.Math.Vector2(this._testerContainer.x, this._testerContainer.y);
+			}
 		});
 	}
 
 	goToMiniGame (): void {
-		this.input.enabled = false;
-		Helper.nextSceneFadeOut(this, 'MiniGameScene');
+		if (this.input.enabled) {
+			this.input.enabled = false;
+			Helper.nextSceneFadeOut(this, 'MiniGameScene');
+		}
 	}
 
 }
