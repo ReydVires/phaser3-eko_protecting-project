848eeeb6633f381cfb2f3c28797b53b2a5fb8cfa
ReydVires <ahmad.fire@gmail.com>
2020-04-20 14:03:27 +0700

    simplify physics sprite class

--- src/scripts/objects/abstract/PhysicSprite.ts
+++ src/scripts/objects/abstract/PhysicSprite.ts
@@ -1,21 +1,13 @@
 export abstract class PhysicSprite extends Phaser.Physics.Arcade.Sprite {
 
-	private _body: Phaser.Physics.Arcade.Body;
-
 	constructor (scene: Phaser.Scene, x: number, y: number, texture: string, isStatic?: boolean) {
 		super(scene, x, y, texture);
 		scene.add.existing(this);
 		scene.physics.add.existing(this, isStatic);
-		this._body = this.body as Phaser.Physics.Arcade.Body;
 	}
 
 	public getBody (): Phaser.Physics.Arcade.Body {
-		return this._body;
-	}
-
-	public setAllowGravity (value: boolean): this {
-		this.getBody().setAllowGravity(value);
-		return this;
+		return this.body as Phaser.Physics.Arcade.Body;
 	}
 
 }
