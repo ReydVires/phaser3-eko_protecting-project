6ea15a3549c8ac2f6bb3b8436d9a8526a36035bd
ReydVires <ahmad.fire@gmail.com>
2020-04-21 14:00:00 +0700

    simplyfy param

--- src/scripts/utils/PhysicsHelper.ts
+++ src/scripts/utils/PhysicsHelper.ts
@@ -1,19 +1,13 @@
-export function OnExitOverlap (gameObject: Phaser.Physics.Arcade.Sprite, callback: Function): void {
-	if (gameObject?.active) {
-		const body = gameObject.body as Phaser.Physics.Arcade.Body;
-		const isAnyOverlap = body.wasTouching.none;
-		if (!isAnyOverlap && body.touching.none) {
-			callback();
-		}
+export function OnExitOverlap (body: Phaser.Physics.Arcade.Body | Phaser.Physics.Arcade.StaticBody, callback: Function): void {
+	const isAnyOverlap = body?.wasTouching.none;
+	if (!isAnyOverlap && body?.touching.none) {
+		callback();
 	}
 }
 
-export function OnEnterOverlap (gameObject: Phaser.Physics.Arcade.Sprite, callback: Function): void {
-	if (gameObject?.active) {
-		const body = gameObject.body as Phaser.Physics.Arcade.Body;
-		const isAnyOverlap = body.wasTouching.none;
-		if (isAnyOverlap && !body.touching.none) {
-			callback();
-		}
+export function OnEnterOverlap (body: Phaser.Physics.Arcade.Body | Phaser.Physics.Arcade.StaticBody, callback: Function): void {
+	const isAnyOverlap = body?.wasTouching.none;
+	if (isAnyOverlap && !body?.touching.none) {
+		callback();
 	}
 }
