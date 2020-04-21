4cde2d301b5dde0b8eb11c7f2e2f93855a8336ee
ReydVires <ahmad.fire@gmail.com>
2020-04-21 14:01:26 +0700

    implement pemukiman asset btn

--- src/scripts/scenes/WorldmapViews.ts
+++ src/scripts/scenes/WorldmapViews.ts
@@ -1,5 +1,6 @@
 import { BaseScene } from "../objects/abstract/BaseScene";
-import { NextSceneFadeOut, FadeIn } from "../utils/Helper";
+import { NextSceneFadeOut, FadeIn, PrintPointerPos } from "../utils/Helper";
+import { FlatButton } from "../objects/components/FlatButton";
 
 export class WorldmapViews extends BaseScene {
 	
@@ -15,8 +16,10 @@ export class WorldmapViews extends BaseScene {
 	create (): void {
 		FadeIn(this, () => { this.input.enabled = true; }, 200);
 		this.add.image(0, 0, 'world_map').setOrigin(0);
+		PrintPointerPos(this);
 
-		this.input.on('pointerup', () => {
+		const villageLevelBtn = new FlatButton(this, 876, 405, 'world_map_pemukiman');
+		villageLevelBtn.setCallback(() => {
 			this.input.enabled = false;
 			NextSceneFadeOut(this, 'TestScene');
 		});
