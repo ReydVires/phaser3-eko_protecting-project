dd1539cdeeae4be71745cd8caeaabf649710c4c8
ReydVires <ahmad.fire@gmail.com>
2020-05-02 04:58:52 +0700

    add new icon button for worldmap

--- src/scripts/scenes/WorldmapViews.ts
+++ src/scripts/scenes/WorldmapViews.ts
@@ -1,6 +1,7 @@
 import { BaseScene } from "../objects/abstract/BaseScene";
-import { NextSceneFadeOut, FadeIn, PrintPointerPos } from "../utils/Helper";
+import { NextSceneFadeOut, FadeIn } from "../utils/Helper";
 import { FlatButton } from "../objects/components/FlatButton";
+import { AndroidBackHelper } from "../utils/AndroidBackHelper";
 
 export class WorldmapViews extends BaseScene {
 	
@@ -17,12 +18,22 @@ export class WorldmapViews extends BaseScene {
 	create (): void {
 		FadeIn(this, () => { this.input.enabled = true; }, 200);
 		this.add.image(0, 0, 'world_map').setOrigin(0);
-		PrintPointerPos(this);
 
 		const villageLevelBtn = new FlatButton(this, 876, 405, 'world_map_pemukiman');
 		villageLevelBtn.setCallback(() => {
 			this.input.enabled = false;
 			NextSceneFadeOut(this, 'GameScene');
+		});
+		this.add.image(villageLevelBtn.x, villageLevelBtn.y + villageLevelBtn.height * 0.5, 'boardname_pemukiman');
+
+		const caveLevelBtn = this.add.image(375, 175, 'world_map_gua_lock');
+		this.add.image(caveLevelBtn.x, caveLevelBtn.y + caveLevelBtn.height * 0.5, 'boardname_gua');
+
+		const beachLevelBtn = this.add.image(313, 515, 'world_map_pantai_lock');
+		this.add.image(beachLevelBtn.x, beachLevelBtn.y + beachLevelBtn.height * 0.5, 'boardname_pantai');
+
+		AndroidBackHelper.Instance.setCallbackBackButton(() => {
+			this.startToScene('MenuViews');
 		});
 	}
 
