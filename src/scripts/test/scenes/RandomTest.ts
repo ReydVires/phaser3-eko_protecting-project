633cb2976c8439f13949b9ff10e3c36d334c241f
ReydVires <ahmad.fire@gmail.com>
2020-05-26 11:54:13 +0700

    test for texture and new font

--- src/scripts/test/scenes/RandomTest.ts
+++ src/scripts/test/scenes/RandomTest.ts
@@ -1,3 +1,6 @@
+import { centerX, centerY } from "../../config";
+import { DialogueBox } from "../../objects/DialogueBox";
+
 export class RandomTest extends Phaser.Scene {
 
 	constructor () {
@@ -7,30 +10,9 @@ export class RandomTest extends Phaser.Scene {
 	init (): void {}
 
 	create (): void {
-		const x = 512;
-		const y = 256;
-		this.add.image(x, y, 'cave_entrance').setOrigin(0, 1);
-
-		const dX = 101 + x;
-		const dY = -51 + y;
-		const door = this.add.image(dX, dY, 'cave_door').setOrigin(0, 1);
-
-		const doorMask = this.add.image(dX, dY, 'cave_door').setOrigin(0, 1).setVisible(false);
-		door.setMask(new Phaser.Display.Masks.BitmapMask(this, doorMask));
-		
-		door.setData('originY', door.y);
-		this.input.on('pointerup', () => {
-			this.tweens.add({
-				targets: door,
-				props: {
-					y: {
-						getStart: () => door.getData('originY')!,
-						getEnd: () => door.y + 100
-					}
-				},
-				duration: 315,
-			});
-		});
+		const text = 'Lorem ipsum dolor sit amet!';
+		const dialogue = new DialogueBox(this, centerX, centerY, '', 'Narrator', text, true);
+		dialogue.setTexture('notify_message');
 	}
 
 }
